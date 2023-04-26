4521ccef0ee08581177299d8f159ce48e2ccb4b6
leomencho <leobollini@gmail.com>
2023-04-26 00:50:59 -0300

    crud funcionando correctamente, ya con todas las pruebas realizadas. Falta adaptar esto a las entradas de archivo, donde estaran fixtura de los partidos y fixtura de los apostadores. Luego habra que comparar esos datos y generar la tabla de los usuarios.

--- src/main/java/org/leobollini/Main.java
+++ src/main/java/org/leobollini/Main.java
@@ -15,8 +15,31 @@ public class Main {
 
     try (Connection conn= ConexionBD.getInstance()) {
         Repositorio<Prueba> repositorio = new ProductoRepositorioImpl();
+        System.out.println("=============listar======================");
         repositorio.listar().forEach(p -> System.out.println(p));
+        System.out.println("=============ver por id======================");
         System.out.println(repositorio.porId(2L));
+        /*System.out.println("============= crear/ insertar ======================");
+        Prueba producto = new Prueba();
+        producto.setNombre("chuchu");
+        producto.setOtro("alfa de la casa");
+        repositorio.guardar(producto);
+        System.out.println("producto guardado con exito");
+
+        System.out.println("============= modificar ======================");
+        Prueba producto = new Prueba();
+        producto.setId(1L);
+        producto.setNombre("vika");
+        producto.setOtro("bollini");
+        repositorio.guardar(producto);
+        System.out.println("producto editador con exito");*/
+
+        System.out.println("========= eliminar =================");
+        repositorio.eliminar(3L);
+        System.out.println("Eliminado con exito");
+
+        repositorio.listar().forEach(System.out::println);
+
     } catch (SQLException e){
         e.printStackTrace();
     }
